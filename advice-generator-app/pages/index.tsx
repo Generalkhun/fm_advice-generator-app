2ebb498bb41c902bf9cdfd08b8351bb0856cafa8
Suttikhun Sihsobhon <suttikhunsihsobhon@Suttikhuns-MacBook-Pro.local>
2022-05-28 17:51:49 +0700

    Add hover effect

--- advice-generator-app/pages/index.tsx
+++ advice-generator-app/pages/index.tsx
@@ -7,6 +7,7 @@ import { AdviceServiceAPIRespose } from './api/advice'
 const Home: NextPage = () => {
   const [adviceMessage, setAdviceMessage] = useState<string>('')
   const [adviceId, setAdviceId] = useState<string>('')
+  const [isGetAdviceBtnHovered, setIsGetAdviceBtnHovered] = useState<boolean>(false)
   const fetchAdviceData = useCallback(async () => {
     const adviceData: AxiosResponse<AdviceServiceAPIRespose> = await axios({
       url: endpointsForFrontend.adviceAPIEndpoints,
@@ -15,6 +16,14 @@ const Home: NextPage = () => {
     setAdviceId(adviceData.data.id)
     setAdviceMessage(adviceData.data.advice)
   }, [])
+  
+  const onHoverBtn = () => {
+    setIsGetAdviceBtnHovered(true);
+  }
+
+  const onUnHoverBtn = () => {
+    setIsGetAdviceBtnHovered(false);
+  }
 
   /**
    * @todo
@@ -28,8 +37,11 @@ const Home: NextPage = () => {
         <p className='mt-12 pb-8 font-sans font-extrabold text-neon-green'>{`ADVICE #${adviceId}`}</p>
         <div className='h-[147px] px-13 text-light-cyan'>{`"${adviceMessage}"`}</div>
         <img className='pb-9' src='/pattern-divider-desktop.svg' />
-        <div className='h-px'>
-          <button className='bg-neon-green rounded-full flex items-center justify-center hover:backdrop-blur-md' style={{ height: '63px', width: '63px' }} onClick={fetchAdviceData}>a</button>
+        <div className='relative h-px'>
+          {isGetAdviceBtnHovered && <div className='absolute h-[63px] w-[63px] bg-neon-green rounded-full blur-lg'></div>}
+          <button onMouseEnter={onHoverBtn} onMouseLeave={onUnHoverBtn} className='relative h-[63px] w-[63px] bg-neon-green rounded-full flex items-center justify-center' onClick={fetchAdviceData}>
+            <img src='/icon-dice.svg' />
+          </button>
         </div>
       </div>
 
