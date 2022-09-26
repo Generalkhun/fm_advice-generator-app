f67c6e3f82e1862424887c183bc888cd13c1bcbb
generalkhun <khun2551@hotmail.com>
2022-09-26 23:22:43 +0700

    Fixing the module not found issue

--- advice-generator-app/config/index.ts
+++
@@ -1,6 +0,0 @@
-export enum endpointsForFrontend {
-    adviceAPIEndpoints = '/api/advice',
-}
-export enum endpointsForBackend {
-    randomAdviceAPIEndpoints = 'https://api.adviceslip.com/advice',
-}
